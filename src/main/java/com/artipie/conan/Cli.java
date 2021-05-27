ee04fb72e0b7e687d83f4df9e5623742307fc6c9
Evgeny Chugunnyy <chgena@mail.ru>
2021-05-27 14:23:31 +0300

    Java 8 compability

--- src/main/java/com/artipie/conan/Cli.java
+++ src/main/java/com/artipie/conan/Cli.java
@@ -5,10 +5,11 @@ import com.artipie.asto.Storage;
 import com.artipie.asto.fs.FileStorage;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public final class Cli {
     public static void main(final String... args) {
-        Path path = Path.of(".");
+        Path path = Paths.get(".");
         Storage storage = new FileStorage(path);
         ConanRepo repo = new ConanRepo(storage, new ConanRepoConfig.Simple());
         repo.batchUpdateIncrementally(Key.ROOT);
